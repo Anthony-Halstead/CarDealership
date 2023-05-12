bf918b908ed21fe40509fd1bff6ddd8515cf1867
tsandbrink <tsandbrink123@gmail.com>
2023-05-12 16:17:28 -0500

    dklas

--- car-dealership-front/src/components/pages/Home.js
+++ car-dealership-front/src/components/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import '../../css/pages/home.css'
+import '../../css/reusables/positions.css'
 function Home() {
   return (
-    <div className='home-content'>Welcome to O.K. Used Cars!
-        <div className='flex-column'>Our name might be O.K. but we only sell the best! Whether you need a car, truck or SUV, we won't rest till you have the best! We're not O.K. unless you're O.K.</div>
+    
+    <div className = 'container background'>
+    <div className='flex-col home-content welcome-box'>Welcome to O.K. Used Cars!
+        <p className='flex-row justify-content-center'>Our name might be O.K. but we only sell the best! Whether you need a car, truck or SUV, we won't rest till you have the best! We're not O.K. unless you're O.K.</p>
+    </div>
     </div>
 
   )
