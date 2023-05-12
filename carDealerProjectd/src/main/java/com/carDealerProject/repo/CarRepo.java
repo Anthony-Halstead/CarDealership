0307632b69b2571d5e0561139c94acffd5a650db
5378772837 <coyotecreekcatahoulas@gmail.com>
2023-05-12 14:15:18 -0500

    Fixed the MySQL querys for auction and non auction cars.

--- carDealerProjectd/src/main/java/com/carDealerProject/repo/CarRepo.java
+++ carDealerProjectd/src/main/java/com/carDealerProject/repo/CarRepo.java
@@ -21,10 +21,10 @@ public interface CarRepo extends JpaRepository<Car, Integer> {
     @Query(value="select * from car where price BETWEEN ?1 AND ?2", nativeQuery = true)
     public List<Car> findByPrice(double fromPrice, double toPrice);
     
-    @Query(value="select * from car where DATE(date_added) > DATE(?1)", nativeQuery = true)
+    @Query(value="select * from car where is_sold = 0 AND DATE(date_added) > DATE(?1)", nativeQuery = true)
     public List<Car> findRegCars(LocalDate date);
     
-    @Query(value="select * from car where DATE(date_added) < DATE(?1)", nativeQuery = true)
+    @Query(value="select * from car where is_sold = 0 AND DATE(date_added) < DATE(?1)", nativeQuery = true)
     public List<Car> findAuctionCars(LocalDate date);
     
     @Query(value="select * from car where is_sold = 0", nativeQuery = true)
